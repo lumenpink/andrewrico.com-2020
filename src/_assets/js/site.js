2f17d545635fa1bffab99eb97e5d5ce577000964
Andrew Rico <andrewric86@gmail.com>
2019-12-25 16:38:04 -0500

    just another commit

--- src/_assets/js/site.js
+++ src/_assets/js/site.js
@@ -12,28 +12,7 @@ Name: el-drawer
 Function: el-drawer 
 Location: [ el-drawer]
 /*--------------------------------------------- */
-const drawer = document.querySelector('el-drawer')
 
-const drawerStyle = {
-	sizePadding: `0.6rem`,
-	brandFontSize: `28px`,
-	brandFontFamily: `'Dancing Script', cursive`,
-	linkFontSize: `1rem`,
-	hoverItem: `rgba(232, 232, 232, 0.69)`,
-	hoverText: `#000`,
-	hoverNavbarText: `var(--color-tertiary)`,
-	backgroundColor: `#fff`,
-	textColor: `#00000`,
-}
-drawer.style.setProperty('--drawer-padding', drawerStyle.sizePadding)
-drawer.style.setProperty('--drawer-brand-font-size', drawerStyle.brandFontSize)
-drawer.style.setProperty('--drawer-brand-font-family', drawerStyle.brandFontFamily)
-drawer.style.setProperty('--drawer-link-font-size', drawerStyle.linkFontSize)
-drawer.style.setProperty('--drawer-hover-color', drawerStyle.hoverItem)
-drawer.style.setProperty('--drawer-hover-text', drawerStyle.hoverText)
-drawer.style.setProperty('--drawer-hover-navbar-text', drawerStyle.hoverNavbarText)
-drawer.style.setProperty('--drawer-background-color', drawerStyle.backgroundColor)
-drawer.style.setProperty('--drawer-text-color', drawerStyle.textColor)
 
 
 
